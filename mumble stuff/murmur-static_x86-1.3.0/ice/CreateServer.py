b2bfcd5e9306480eef7f3eccd27464ebf3d41411
root <root@localhost.localdomain>
2020-05-10 13:41:15 +0100

    initial commit

---
+++ mumble stuff/murmur-static_x86-1.3.0/ice/CreateServer.py
@@ -0,0 +1,8 @@
+import mice
+
+numOfNewServers = 
+for num in range(numOfNewServers):
+	mice.m.newServer()
+print("There are now " + len(mice.m.getAllServers()) + " servers."
+
+numOfServersToDelete()
